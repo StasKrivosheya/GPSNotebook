bb77c0f68b57a0ee66325d1fc16e61cd64f4155e
Stas Kryvosheia <stask49@gmail.com>
2021-03-30 13:28:36 +0300

    Created basic tabbed Xamarin.Forms application

---
+++ GPSNotebook/GPSNotebook/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using GPSNotebook.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace GPSNotebook.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
