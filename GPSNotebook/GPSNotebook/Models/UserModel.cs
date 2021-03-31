e613f39f73390374e31544fca61f0835236e05e3
Stas Kryvosheia <stask49@gmail.com>
2021-03-31 16:25:29 +0300

    Added IEntityBase and UserModel

---
+++ GPSNotebook/GPSNotebook/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+
+namespace GPSNotebook.Models
+{
+    [Table("Users")]
+    public class UserModel : IEntityBase
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [Unique]
+        public string Mail { get; set; }
+
+        [MaxLength(16)]
+        public string Password { get; set; }
+    }
+}
