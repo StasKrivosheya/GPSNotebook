713456f5f03e826cf9de6b92589ff385fdc2509f
Stas Kryvosheia <stask49@gmail.com>
2021-03-31 16:26:59 +0300

    Created Generic repository and UserService

---
+++ GPSNotebook/GPSNotebook/Services/UserService/IUserService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using GPSNotebook.Models;
+
+namespace GPSNotebook.Services.UserService
+{
+    public interface IUserService
+    {
+        Task<List<UserModel>> GetItemsAsync();
+
+        Task<UserModel> GetItemAsync(int id);
+        Task<UserModel> GetItemAsync(Expression<Func<UserModel, bool>> predicate);
+
+        Task<int> InsertItemAsync(UserModel item);
+        Task<int> UpdateItemAsync(UserModel item);
+
+        Task<int> DeleteItemAsync(UserModel item);
+    }
+}
