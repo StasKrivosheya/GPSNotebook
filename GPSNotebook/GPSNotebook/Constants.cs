f1f85a577369629a2681437cddd8b8dfb6b66cd8
Stas Kryvosheia <stask49@gmail.com>
2021-03-31 16:28:28 +0300

    Created validators for mail and passwords, added Constants.cs

---
+++ GPSNotebook/GPSNotebook/Constants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GPSNotebook
+{
+    public abstract class Constants
+    {
+        public const string DATABASE_NAME = "gps_notebook.db";
+        public static string DatabasePath => Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+    }
+}
