bd6491b99f1e9a90703da89115c56468bd5ae6e6
Stas Kryvosheia <stask49@gmail.com>
2021-03-31 16:33:32 +0300

    Added SignUpViewModel and SignInViewModel

---
+++ GPSNotebook/GPSNotebook/ViewModels/SignInViewModel.cs
@@ -0,0 +1,131 @@
+﻿using System.ComponentModel;
+using Acr.UserDialogs;
+using GPSNotebook.Resources;
+using GPSNotebook.Services.Authorization;
+using GPSNotebook.Services.UserService;
+using GPSNotebook.Views;
+using Prism.Commands;
+using Prism.Navigation;
+using Xamarin.Forms;
+
+namespace GPSNotebook.ViewModels
+{
+    public class SignInViewModel : ViewModelBase
+    {
+        private readonly IAuthorizationService _authorizationService;
+        private readonly IUserService _userService;
+
+        #region --- Constructors ---
+
+        public SignInViewModel(INavigationService navigationService,
+            IUserService userService,
+            IAuthorizationService authorizationService) :
+            base(navigationService)
+        {
+            Title = Resource.SignInTitle;
+
+            _userService = userService;
+
+            _authorizationService = authorizationService;
+        }
+
+        #endregion
+
+        #region --- Public Properties ---
+
+        private DelegateCommand _navigateCommand;
+        public DelegateCommand NavigateCommand =>
+            _navigateCommand ?? (_navigateCommand = new DelegateCommand(ExecuteNavigateCommand));
+
+        private DelegateCommand _signInCommand;
+        public DelegateCommand SignInCommand =>
+            _signInCommand ?? (_signInCommand = new DelegateCommand(ExecuteSignInCommand));
+
+        private bool _isButtonEnabled;
+        public bool IsButtonEnabled
+        {
+            get => _isButtonEnabled;
+            set => SetProperty(ref _isButtonEnabled, value);
+        }
+
+        private string _mail;
+        public string Mail
+        {
+            get => _mail;
+            set => SetProperty(ref _mail, value);
+        }
+
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set => SetProperty(ref _password, value);
+        }
+
+        #endregion
+
+        #region --- Overrides ---
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            if (parameters.TryGetValue(nameof(Mail), out string mail))
+            {
+                Mail = mail;
+                Password = string.Empty;
+            }
+        }
+
+        protected override void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+            base.OnPropertyChanged(args);
+
+            if (args.PropertyName == nameof(Password) ||
+                args.PropertyName == nameof(Mail))
+            {
+                UpdateSignInButtonState();
+            }
+        }
+
+        #endregion
+
+        #region --- Command Handlers ---
+
+        private async void ExecuteNavigateCommand()
+        {
+            await NavigationService.NavigateAsync(nameof(SignUpPage));
+        }
+
+        private async void ExecuteSignInCommand()
+        {
+            var query = await _userService.GetItemAsync(u =>
+                u.Mail.Equals(Mail) && u.Password.Equals(Password));
+
+            if (query != null)
+            {
+                _authorizationService.Authorize(query.Id, query.Mail);
+
+                await NavigationService.NavigateAsync($"/{nameof(NavigationPage)}" +
+                                                      $"/{nameof(HomePage)}");
+            }
+            else
+            {
+                await UserDialogs.Instance.AlertAsync(Resource.SignInErrorMessage,
+                    Resource.SignInErrorTitle,
+                    Resource.OkText);
+                Password = string.Empty;
+            }
+        }
+
+        #endregion
+
+        #region --- Private Helpers ---
+
+        private void UpdateSignInButtonState()
+        {
+            IsButtonEnabled = !string.IsNullOrEmpty(Mail) &&
+                              !string.IsNullOrEmpty(Password);
+        }
+
+        #endregion
+    }
+}
---
+++ GPSNotebook/GPSNotebook/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,153 @@
+﻿using System.ComponentModel;
+using Acr.UserDialogs;
+using GPSNotebook.Models;
+using GPSNotebook.Resources;
+using GPSNotebook.Services.UserService;
+using GPSNotebook.Validators;
+using Prism.Commands;
+using Prism.Navigation;
+
+namespace GPSNotebook.ViewModels
+{
+    public class SignUpViewModel : ViewModelBase
+    {
+        private readonly IUserService _userService;
+
+        public SignUpViewModel(INavigationService navigationService,
+            IUserService userService) :
+            base(navigationService)
+        {
+            Title = Resource.SignUpTitle;
+
+            _userService = userService;
+        }
+
+        #region --- Public Properties
+
+        private DelegateCommand _signUpCommand;
+        public DelegateCommand SignUpCommand =>
+            _signUpCommand ?? (_signUpCommand = new DelegateCommand(ExecuteSignUpCommand));
+
+        private bool _isButtonEnabled;
+        public bool IsEnabled
+        {
+            get => _isButtonEnabled;
+            private set => SetProperty(ref _isButtonEnabled, value);
+        }
+
+        private string _mail;
+        public string Mail
+        {
+            get => _mail;
+            set => SetProperty(ref _mail, value);
+        }
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
+
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set => SetProperty(ref _password, value);
+        }
+
+        private string _confirmPassword;
+        public string ConfirmPassword
+        {
+            get => _confirmPassword;
+            set => SetProperty(ref _confirmPassword, value);
+        }
+
+        #endregion
+
+        #region --- Overrides ---
+
+        protected override void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+            base.OnPropertyChanged(args);
+
+            if (args.PropertyName == nameof(Password) ||
+                args.PropertyName == nameof(ConfirmPassword) ||
+                args.PropertyName == nameof(Mail) ||
+                args.PropertyName == nameof(Name))
+            {
+                UpdateSignUpButtonState();
+            }
+        }
+
+        #endregion
+
+        #region --- Command Handlers ---
+
+        private async void ExecuteSignUpCommand()
+        {
+            if (Password == ConfirmPassword)
+            {
+                var loginValid = StringValidator.Validate(Mail, StringValidator.Mail);
+                var passwordValid = StringValidator.Validate(Password, StringValidator.Password);
+
+                if (!loginValid)
+                {
+                    UserDialogs.Instance.Alert(Resource.InvalidMailMessage,
+                        Resource.InvalidMailTitle,
+                        Resource.OkText);
+                }
+                else if (!passwordValid)
+                {
+                    UserDialogs.Instance.Alert(Resource.InvalidPasswordMessage,
+                        Resource.InvalidPasswordTitle,
+                        Resource.OkText);
+                }
+                else
+                {
+                    var userModel = new UserModel
+                    {
+                        Mail = Mail,
+                        Name = Name,
+                        Password = Password
+                    };
+
+                    var result = await _userService.InsertItemAsync(userModel);
+
+                    if (result == -1)
+                    {
+                        UserDialogs.Instance.Alert(Resource.RegisterFailedMessage,
+                            Resource.RegisterFailedTitle,
+                            Resource.OkText);
+                    }
+                    else
+                    {
+                        var parameters = new NavigationParameters { { nameof(Mail), Mail } };
+                        await NavigationService.GoBackAsync(parameters);
+                    }
+                }
+            }
+            else
+            {
+                UserDialogs.Instance.Alert(Resource.DifferentPasswordError,
+                    Resource.DifferentPasswordError,
+                    Resource.OkText);
+            }
+        }
+
+        #endregion
+
+        #region --- Private Helpers ---
+
+        private void UpdateSignUpButtonState()
+        {
+            bool allEntriesAreFilled = !string.IsNullOrEmpty(_mail) &&
+                                       !string.IsNullOrEmpty(_password) &&
+                                       !string.IsNullOrEmpty(_confirmPassword);
+
+            IsEnabled = allEntriesAreFilled;
+        }
+
+        #endregion
+    }
+}
