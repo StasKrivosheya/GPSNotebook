ed5e6be6ae0ac38d1477c9ead9d2d6759b3f813d
Stas Kryvosheia <stask49@gmail.com>
2021-04-01 22:25:24 +0300

    Improved toolbar design on tabbed page

---
+++ GPSNotebook/GPSNotebook/ViewModels/MapTabViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Essentials;
+using Xamarin.Forms.GoogleMaps;
+using GoogleMap = Xamarin.Forms.GoogleMaps.Map;
+
+namespace GPSNotebook.ViewModels
+{
+    class MapTabViewModel
+    {
+        GoogleMap MainMap = new GoogleMap();
+        
+        async void ShowLocation()
+        {
+
+            await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            MainMap.MyLocationEnabled = true;
+            var position = await Geolocation.GetLocationAsync();
+            MainMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(position.Latitude, position.Longitude),
+                Distance.FromMiles(1)));
+        }
+    }
+}
