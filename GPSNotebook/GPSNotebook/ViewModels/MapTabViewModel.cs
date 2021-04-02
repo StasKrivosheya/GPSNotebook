9d8c83f7c69a93f54ac542c914a8e50d86273132
Stas Kryvosheia <stask49@gmail.com>
2021-04-02 23:55:03 +0300

    Added custom map with basic property with the simple sample in MapTabViewModel

--- GPSNotebook/GPSNotebook/ViewModels/MapTabViewModel.cs
+++ GPSNotebook/GPSNotebook/ViewModels/MapTabViewModel.cs
@@ -1,21 +1,67 @@
-﻿using Xamarin.Essentials;
+﻿using System.Collections.ObjectModel;
+using Prism.Navigation;
+using Xamarin.Essentials;
 using Xamarin.Forms.GoogleMaps;
 using GoogleMap = Xamarin.Forms.GoogleMaps.Map;
 
 namespace GPSNotebook.ViewModels
 {
-    class MapTabViewModel
-    {
-        GoogleMap MainMap = new GoogleMap();
-        
-        async void ShowLocation()
-        {
+    // --- contains lots of tmp stuff just for debug or trying  ---
+    // --- will be replaced with useful code in further commits ---
 
+    public class MapTabViewModel : ViewModelBase
+    {
+        public MapTabViewModel(
+            INavigationService navigationService)
+            : base(navigationService)
+        {
+            PinsList = new ObservableCollection<Pin>
+            {
+                new Pin {Label = "Pin1", Position = new Position(0, 0)},
+                new Pin {Label = "Rome", Position = new Position(41.902782, 12.496366)}
+            };
+        }
+
+        #region -- Public properties --
+
+        private ObservableCollection<Pin> _pinsList;
+
+        public ObservableCollection<Pin> PinsList
+        {
+            get => _pinsList;
+            set => SetProperty(ref _pinsList, value);
+        }
+
+        #endregion
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+
+        }
+
+        public override void Initialize(INavigationParameters parameters)
+        {
+            base.Initialize(parameters);
+        }
+
+        #region -- TMP SandBox --
+
+        private GoogleMap _mainMap;
+        public GoogleMap MainMap
+        {
+            get => _mainMap;
+            set => SetProperty(ref _mainMap, value);
+        }
+
+        async void NavigateToLocation()
+        {
             await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
             MainMap.MyLocationEnabled = true;
             var position = await Geolocation.GetLocationAsync();
             MainMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(position.Latitude, position.Longitude),
                 Distance.FromMiles(1)));
         }
+        #endregion
     }
 }
